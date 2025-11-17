a713e68d76aea8f1fe919cd8c2bd36bfaab6ca14
Pessi-L <pessibr@gmail.com>
2025-11-18 01:49:05 +0200

    Add detect_main_face function to the abstract class for selecting main face from multiple detections

--- app/core/preprocess/face_detectors/base_detector.py
+++ app/core/preprocess/face_detectors/base_detector.py
@@ -2,10 +2,43 @@ from abc import ABC, abstractmethod
 
 class BaseFaceDetector(ABC):
     @abstractmethod
-    def detect_faces(self, image):
+    def detect_faces(self, image, mode='image'):
         """
         :param image: numpy array
+        :param mode: optional modes: image, video, live_stream
         :return:
         """
         pass
+
+    def detect_main_face(self, faces, img_shape):
+        """
+        Detects the main face in an image
+
+        Logic:
+        - Pick the largest bounding box by area.
+        - If multiple boxes are similar in size, choose the one closest to the image center.
+
+        :param faces: numpy array
+        :param img_shape: (x, y)
+        :return:
+        """
+
+        h, w = img_shape
+        cx, cy = w/2, h/2
+
+        # Sort by are descending
+        faces_sorted = sorted(faces, key=lambda box: box[2] * box[3], reverse=True)
+        largest_area = faces_sorted[0][2] * faces_sorted[0][3]
+
+        # Filter faces with area within 90% of largest
+        candidates = [box for box in faces_sorted if box[2] * box[3] >= 0.9 * largest_area]
+
+        # Pick the candidate closest to the center
+        def distance_to_center(box):
+            bx, by, bw, bh = box
+            return (((bx + bw/2) - cx)**2 + ((by + bh/2) - cy)**2)**0.5
+
+        main_face = min(candidates, key=distance_to_center)
+        return main_face
+
 
